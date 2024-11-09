460e440cc186d5c29a8fe877aadee251d1924c7c
romilk-senpai <rpakhomov1702@gmail.com>
2024-11-09 22:01:22 +0200

    removed unnecessary stuff from map test

--- test/map_test.c
+++ test/map_test.c
@@ -2,13 +2,11 @@
 #include <stdio.h>
 
 uint32_t string_hash(void *key);
-/*void read_set1();*/
 void test_ref_type();
 
 int main(void) {
   printf("---map_test---\n");
 
-  /*read_set1();*/
   test_ref_type();
 
   printf("---map_test---\n");
@@ -27,11 +25,6 @@ uint32_t string_hash(void *key) {
 
   return hash;
 }
-
-/*void print_map_kvp(key_value_pair *kvp, void *context) {*/
-/*  printf("%d, %s: %d\n", *(int *)context, (char *)kvp->key, *(int
- * *)kvp->value);*/
-/*}*/
 
 void print_map_kvp2(key_value_pair *kvp, void *context) {
   printf("%d:%s\n", *(int *)kvp->key, (char *)kvp->value);
@@ -57,76 +50,3 @@ void test_ref_type() {
 
   printf("Element at %d, %s\n", i1, (char *)hash_map_get(map, &i1));
 }
-
-/*void read_set1() {*/
-/*  hash_map *map = hash_map_create_cap(1000, string_hash);*/
-/**/
-/*  FILE *file = fopen("out/test/map_test_set1.txt", "rb");*/
-/*  if (file == NULL) {*/
-/*    fprintf(stderr, "Error opening file t1\n");*/
-/*    return;*/
-/*  }*/
-/*  char *buf = malloc(sizeof(char) * 1024);*/
-/*  size_t buf_index = 0;*/
-/*  char ch;*/
-/*  while ((ch = fgetc(file)) != EOF) {*/
-/*    if (ch == 10 || ch == 32) {*/
-/*      if (buf_index == 0)*/
-/*        continue;*/
-/**/
-/*      buf[buf_index] = '\0';*/
-/*      char *key = strdup(buf);*/
-/*      if (hash_map_contains(map, key) == 0) {*/
-/*        hash_map_set(map, key, 1);*/
-/*      } else {*/
-/*        int val = hash_map_get(map, int, key);*/
-/*        hash_map_set(map, key, val + 1);*/
-/*      }*/
-/**/
-/*      buf_index = 0;*/
-/*      continue;*/
-/*    }*/
-/**/
-/*    buf[buf_index] = ch;*/
-/*    buf_index++;*/
-/*  }*/
-/**/
-/*  if (buf_index > 0) {*/
-/*    buf[buf_index] = '\0';*/
-/*    char *key = strdup(buf);*/
-/*    if (hash_map_contains(map, key) == 0) {*/
-/*      hash_map_set(map, key, 1);*/
-/*    } else {*/
-/*      int val = hash_map_get(map, int, key);*/
-/*      hash_map_set(map, key, val + 1);*/
-/*    }*/
-/*  }*/
-/**/
-/*  fclose(file);*/
-/**/
-/*  if (hash_map_contains(map, "banana") == 1) {*/
-/*    int val = hash_map_get(map, int, "banana");*/
-/*    printf("%s: %d\n", "banana", val); // expect 12*/
-/*  }*/
-/*  if (hash_map_contains(map, "lemon") == 1) {*/
-/*    int val = hash_map_get(map, int, "lemon");*/
-/*    printf("%s: %d\n", "lemon", val); // expect 9*/
-/*  }*/
-/*  if (hash_map_contains(map, "watermelon") == 1) {*/
-/*    int val = hash_map_get(map, int, "watermelon");*/
-/*    printf("%s: %d\n", "watermelon", val); // expect 3*/
-/*  }*/
-/**/
-/*  printf("-------------\n");*/
-/**/
-/*  int *sample_context;*/
-/*  *sample_context = 7;*/
-/**/
-/*  hash_map_for_each(map, print_map_kvp, sample_context);*/
-/**/
-/*  printf("Size: %zu; Capacity: %zu\n", map->size,*/
-/*         map->capacity); // expect 13/20*/
-/**/
-/*  free(buf);*/
-/*  hash_map_free(map);*/
-/*}*/
